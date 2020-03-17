135fb64997c29a4a6c4478172924a1ba538577aa
cjaewon <jwn4492@outlook.com>
2020-03-18 08:58:43 +0900

    Update nodejs.yml & app.ts

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import discord from './lib/discord';
 // import slack from './lib/slack';
 
 (async () => {
+  core.debug('Hello World');
   const WEBHOOKS = core.getInput('WEBHOOKS');
   if (WEBHOOKS == null) throw new Error('웹훅을 찾을 수 없어요.');
   const webhookList = WEBHOOKS.split(',');
