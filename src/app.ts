9a8422bb7c537bb0de1c918aecc14299491c37c7
cjaewon <jwn4492@outlook.com>
2020-03-18 00:06:52 +0900

    Add Error logger

--- src/app.ts
+++ src/app.ts
@@ -4,14 +4,14 @@ import discord from './lib/discord';
 // import slack from './lib/slack';
 
 (async () => {
-  const WEBHOOKS = core.getInput('WEBHOOKS') ;
+  const WEBHOOKS = core.getInput('WEBHOOKS');
+  if (WEBHOOKS == null) throw new Error('웹훅을 찾을 수 없어요.');
   const webhookList = WEBHOOKS.split(',');
 
   const parsed = await parser();
 
   webhookList.map(async url => {
     if (url.includes('discordapp.com')) { // discord webhook
-      console.log(parsed);
       await discord({
         ...parsed,
         url
